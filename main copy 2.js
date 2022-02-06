5622ea602452e9d5cc9370658acb64f850a4e7cd
Rakshit-Varshney <75945445+Rakshit-Varshney@users.noreply.github.com>
2022-02-06 11:57:37 -0500

    Add files via upload

---
+++ main copy 2.js
@@ -0,0 +1,43 @@
+img="";
+status1="";
+objects=[];
+function preload(){
+img=loadImage("tv.jpeg")
+}
+
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="status:detecting objects";
+}
+    
+function modelLoaded(){
+        console.log("model loaded");
+    status1=true;
+    objectDetector.detect(img,gotResult);
+}
+    
+function gotResult(error,results){
+        if(error){
+            console.log(error);
+        }
+        console.log(results);
+        objects=results;
+}
+
+function draw(){
+    image(img,0,0,640,420);
+    
+    if (status1 != ""){
+        for(i=0;i<objects.length;i++){
+            document.getElementById("status").innerHTML="status:objects detected";
+            fill("#ff0000");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
+            noFill();
+            stroke("#ff0000");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
+    }
